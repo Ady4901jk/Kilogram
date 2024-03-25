26ee7f5670e7a04cafc0cfbf2fc788cf890ae6a1
Aditya <aditya4901j@gmail.com>
2024-03-26 01:55:13 +0530

    emoji close functionality

--- client/src/components/Chat-Threads/MessageInp.tsx
+++ client/src/components/Chat-Threads/MessageInp.tsx
@@ -1,35 +1,54 @@
-"use client";
 import { useSelectedUser } from "@/store/userStore";
 import { SendMsIcon, SmileFaceIcon } from "@/utils/icons";
 import dynamic from "next/dynamic";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { io } from "socket.io-client";
-import{URL} from "@/Url";
+import { URL } from "@/Url";
+
 const Picker = dynamic(
-    () => {
-        return import("emoji-picker-react");
-    },
-    {ssr:false}
-)
+  () => {
+    return import("emoji-picker-react");
+  },
+  { ssr: false }
+);
 
 function MessageInp() {
   const [inpValue, setInpValue] = useState<string>("");
-    const [showEmojies, setShowEmojies] = useState<boolean>(false);
-    const selectedUser = useSelectedUser((state) => state.selectedUser);
-    const [cookie,setCookie]=useCookies(["user"])
-    const socket=io(URL)
+  const [showEmojies, setShowEmojies] = useState<boolean>(false);
+  const selectedUser = useSelectedUser((state) => state.selectedUser);
+  const [cookie, setCookie] = useCookies(["user"]);
+  const socket = io(URL);
 
-    function handleSubmit(e: { preventDefault: () => void; }) {
-        e.preventDefault();
-        socket.emit("private message",selectedUser.email,inpValue,cookie.user)
-        setInpValue("")
-    }
-    
-    function onEmojiClick(emojiObject:{emoji:string}) {
-        setInpValue(pre=>pre + emojiObject.emoji)
-    }
-    
+  const emojiPickerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        emojiPickerRef.current &&
+        !emojiPickerRef.current.contains(event.target as Node)
+      ) {
+        setShowEmojies(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  function handleSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    socket.emit("private message", selectedUser.email, inpValue, cookie.user);
+    setInpValue("");
+  }
+
+  function onEmojiClick(emojiObject: { emoji: string }) {
+    setInpValue((prev) => prev + emojiObject.emoji);
+  }
+
   return (
     <form className="mt-auto relative" onSubmit={handleSubmit}>
       <div className="w-full relative">
@@ -44,17 +63,19 @@ function MessageInp() {
       <button
         type="button"
         onClick={() => setShowEmojies(!showEmojies)}
-        className="absolute top-1/2 left-5 -translate-y-1/2">
+        className="absolute top-1/2 left-5 -translate-y-1/2"
+      >
         <SmileFaceIcon />
       </button>
       {showEmojies && (
-        <div className="absolute bottom-full">
+        <div ref={emojiPickerRef} className="absolute bottom-full">
           <Picker onEmojiClick={onEmojiClick} />
         </div>
       )}
       <button
         type="submit"
-        className="absolute top-1/2 right-5 -translate-y-1/2">
+        className="absolute top-1/2 right-5 -translate-y-1/2"
+      >
         <SendMsIcon />
       </button>
     </form>
