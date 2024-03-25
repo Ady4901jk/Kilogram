1295d19ac089d28db7a73f5e6416e20bf7857bf1
Aditya <aditya4901j@gmail.com>
2024-03-26 03:04:53 +0530

    solved

--- server/index.ts
+++ server/index.ts
@@ -11,36 +11,36 @@ import jwt from "jsonwebtoken";
 const app = express();
 const server = http.createServer(app);
 const { PORT } = process.env || 4000;
-
 app.use(cors({
-  origin: "kilogram-gqndd9kdn-adityas-projects-6d993e50.vercel.app" 
+  origin: "https://kilogram-gqndd9kdn-adityas-projects-6d993e50.vercel.app" 
 }));
 app.use(express.json());
-
 mongoose.connect(process.env.MONGO_URL!);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("connected to mongodb");
   server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:4000`);
+    console.log("http://localhost:4000");
   });
 });
+
 
 const io = new Server(server, {
   cors: {
-    origin: "kilogram-gqndd9kdn-adityas-projects-6d993e50.vercel.app" // Add your deployed frontend URL here
+    origin:"https://kilogram-gqndd9kdn-adityas-projects-6d993e50.vercel.app"
   }
-});
+})
 
 io.on("connection", (socket) => {
-  socket.on("joined", () => {
-    io.sockets.emit("new-user", "new user joined");
-  });
 
+  socket.on("joined", () => {
+    io.sockets.emit("new-user","new user joined")
+  })
+  
   socket.on("private message", async (to, message, mySelf) => {
     const user = await User.find({ email: to });
-    const decoded = jwt.verify(mySelf, process.env.ACCESS_TOKEN_SECRET!);
+    const decoded =jwt.verify(mySelf, process.env.ACCESS_TOKEN_SECRET!);
     const sender = await User.findById(decoded);
     io.sockets.emit("refresh", "new Message");
 
@@ -49,20 +49,20 @@ io.on("connection", (socket) => {
         reciver: user[0].email,
         message,
         sender: sender?.email,
-        time: new Date()
-      });
+        time:new Date()
+      })
       sender?.messages.push({
         reciver: user[0].email,
         message,
         sender: sender?.email,
-        time: new Date()
-      });
+        time:new Date()
+      })
       await user[0].save();
       await sender?.save();
     }
-  });
-});
+  })
+
+})
+
 
 app.use("/", userRouter);
-
-export default server;
